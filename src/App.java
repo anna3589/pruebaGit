4e0f247817bb805ba42354d91c4faa488fcb7011
Anna Batuieva <ikdgv@plaiaundi.net>
2024-12-11 10:42:17 +0100

    commitMelissa

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Rama Nueva! ");
-        System.out.println("Hello, Jokin! ");
+        System.out.println("Hello, Melissa! ");
     }
 }
