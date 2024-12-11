3f2bfb4019935d65d700e641d9232d4b83e996f8
Anna Batuieva <ikdgv@plaiaundi.net>
2024-12-11 10:28:35 +0100

    commitJokin

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Rama Nueva! ");
+        System.out.println("Hello, Jokin! ");
     }
 }
